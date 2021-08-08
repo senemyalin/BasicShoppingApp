84e36ad890ed217d3aabc1af91cfbf0ab3ee7ddd
Senem Yalın <senemyalin@hotmail.com>
2021-08-08 16:08:38 +0300

    Update

--- app/src/main/java/com/example/basicshoppingapp/MainActivity.java
+++ app/src/main/java/com/example/basicshoppingapp/MainActivity.java
@@ -1,20 +1,62 @@
 package com.example.basicshoppingapp;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Product> products = new ArrayList<>();
+    RecyclerView categoryList;
+    List<String> categoryName;
+    List<Integer> categoryImage;
+    List<String> categoryDescription;
+    //List<String> categoryGoButton;
+
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        categoryList = findViewById(R.id.recyclerView_category);
+
+        categoryName = new ArrayList<>();
+        categoryImage = new ArrayList<>();
+        categoryDescription = new ArrayList<>();
+
+        categoryName.add("Vegan");
+        categoryName.add("Fresh Bakery");
+        categoryName.add("Snacks");
+        categoryName.add("Baverages");
+        categoryName.add("Fruits & Veg");
+        categoryName.add("Fit & Form");
+
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+        categoryImage.add(R.mipmap.ic_category_vegan_round);
+
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+        categoryDescription.add("This is an category explanation. It is added because of trying. I wanted to add two more sentences. That is why I kept typing. That is it!");
+
+        categoryAdapter = new CategoryAdapter(this, categoryName, categoryImage, categoryDescription);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL,false);
+
+        categoryList.setLayoutManager(gridLayoutManager);
+        categoryList.setAdapter(categoryAdapter);
     }
 }
